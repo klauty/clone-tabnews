7591e168f839f40e02915bb01e10b9252457cb61
klauty <kaue.developer@gmail.com>
2024-08-27 16:25:00 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1> Amo voce Joyce 😘,separei esse pedacinho da internet pra voce </h1>
+    return <h1> Amo voce Joyce 😘,separei esse pedacinho da internet pra voce ! </h1>
 }
 
 export default Home;
